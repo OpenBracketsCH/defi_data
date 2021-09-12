f76464ba4724fe77f07c235ae667c22824928bf5
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-09-12 20:25:59 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -4,3 +4,9 @@ df.to_csv (r'data/csv/defis_ch_24h.csv', index = None)
 
 df = pd.read_json (r'data/json/defis_ch_not_24h.geojson')
 df.to_csv (r'data/csv/defis_ch_not_24h.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_dispo_knzsg.geojson')
+df.to_csv (r'data/csv/defis_dispo_knzsg.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_dispo_srz.geojson')
+df.to_csv (r'data/csv/defis_dispo_srz.csv', index = None)
