5f14ec17d52b6e9ab751d16bf6dd3f7f26ab2881
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-09-12 20:23:28 +0200

    Update run_converter.sh

--- run_converter.sh
+++ run_converter.sh
@@ -11,6 +11,6 @@ trap "cleanup" EXIT
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo -ne "Konvertiere Daten           "
-cat python $DIR/converter.py
+python $DIR/converter.py
 echo -ne "\t\t - Done.\r"
 echo ""
