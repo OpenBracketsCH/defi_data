3ef35b8bdd8e9f0cf2c7278c040c7c5b6e7aa95a
Christian Nüssli <63254403+chnuessli@users.noreply.github.com>
2021-09-13 11:17:34 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -10,3 +10,96 @@ df.to_csv (r'data/csv/defis_dispo_knzsg.csv', index = None)
 
 df = pd.read_json (r'data/json/defis_dispo_srz.geojson')
 df.to_csv (r'data/csv/defis_dispo_srz.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ag.geojson')
+df.to_csv (r'data/csv/defis_kt_ag.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ai.geojson')
+df.to_csv (r'data/csv/defis_kt_ai.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ar.geojson')
+df.to_csv (r'data/csv/defis_kt_ar.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_be.geojson')
+df.to_csv (r'data/csv/defis_kt_be.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_bl.geojson')
+df.to_csv (r'data/csv/defis_kt_bl.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_bs.geojson')
+df.to_csv (r'data/csv/defis_kt_bs.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_fr.geojson')
+df.to_csv (r'data/csv/defis_kt_fr.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ge.geojson')
+df.to_csv (r'data/csv/defis_kt_ge.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_gl.geojson')
+df.to_csv (r'data/csv/defis_kt_gl.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_gr.geojson')
+df.to_csv (r'data/csv/defis_kt_gr.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ju.geojson')
+df.to_csv (r'data/csv/defis_kt_ju.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_lu.geojson')
+df.to_csv (r'data/csv/defis_kt_lu.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ne.geojson')
+df.to_csv (r'data/csv/defis_kt_ne.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_nw.geojson')
+df.to_csv (r'data/csv/defis_kt_nw.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ow.geojson')
+df.to_csv (r'data/csv/defis_kt_ow.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_sg.geojson')
+df.to_csv (r'data/csv/defis_kt_sg.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_sh.geojson')
+df.to_csv (r'data/csv/defis_kt_sh.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_so.geojson')
+df.to_csv (r'data/csv/defis_kt_so.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_sz.geojson')
+df.to_csv (r'data/csv/defis_kt_sz.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_tg.geojson')
+df.to_csv (r'data/csv/defis_kt_tg.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ti.geojson')
+df.to_csv (r'data/csv/defis_kt_ti.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_ur.geojson')
+df.to_csv (r'data/csv/defis_kt_ur.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_vd.geojson')
+df.to_csv (r'data/csv/defis_kt_vd.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_vs.geojson')
+df.to_csv (r'data/csv/defis_kt_vs.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_zg.geojson')
+df.to_csv (r'data/csv/defis_kt_zg.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_kt_zh.geojson')
+df.to_csv (r'data/csv/defis_kt_zh.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_li_24h.geojson')
+df.to_csv (r'data/csv/defis_li_24h.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_li_not_24h.geojson')
+df.to_csv (r'data/csv/defis_li_not_24h.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_switzerland.geojson')
+df.to_csv (r'data/csv/defis_switzerland.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_liechtenstein.geojson')
+df.to_csv (r'data/csv/defis_liechtenstein.csv', index = None)
+
+df = pd.read_json (r'data/json/defis_stadt_zh.geojson')
+df.to_csv (r'data/csv/defis_stadt_zh.csv', index = None)
